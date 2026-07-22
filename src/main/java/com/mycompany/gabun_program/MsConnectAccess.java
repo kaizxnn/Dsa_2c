857ecd33fcf70e9d41f97cae392ec77325d1d201
CL2-PC <CL2-PC@CL2-PC27>
2026-07-22 14:40:05 +0800

    frame

---
+++ src/main/java/com/mycompany/gabun_program/MsConnectAccess.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.gabun_program;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+
+/**
+ *
+ * @author CL2-PC
+ */
+public class MsConnectAccess {
+    public static Connection conn() {
+        try {
+            String url = "jdbc:ucanaccess://C:/Users/CL2-PC/Documents/gabgab.accdb";
+            Connection conn = DriverManager.getConnection(url);
+            return conn;
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e);
+        }
+        return null;
+
+    
+    }
+}
+
